6da93210108649c200d3136a27eb9bb09c637fdf
coathanger1234 <47996665+coathanger1234@users.noreply.github.com>
2019-04-11 11:02:13 +1000

    Update Product.java

--- Product.java
+++ Product.java
@@ -5,9 +5,39 @@ public class Product
 	private double weight;
 	private int quantity;
 	
-	//add comments
+	public product{
+		name = "";
+		price = 0.0;
+		weight = 0.0;
+		quantity = 0;
+	}
+	
+	//Getters
 	public String getName(){
-		//...
+		return name;
+	}
+	public double getPrice(){
+		return price;
+	}
+	public double getWeight(){
+		return weight;
+	}
+	public int getQuantity(){
+		return quantity;
+	}
+	
+	//Setters
+	public setName(String x){
+		name = x;
+	}
+	public setPrice(double x){
+		price = x;
+	}
+	public setWeight(double x){
+		weight = x;
+	}
+	public setQuantity(int x){
+		quantity = x;
 	}
 	//add other methods
 }
