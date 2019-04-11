3639754f9d8344e94a012983e2732dc71615aa69
coathanger1234 <47996665+coathanger1234@users.noreply.github.com>
2019-04-11 11:04:50 +1000

    Update Depot.java

--- Depot.java
+++ Depot.java
@@ -3,10 +3,11 @@ public class Depot
 	private String name;
 	private Product product1, product2, product3;
 	
-	//add comments
+	//Getter
 	public String getName(){
 		return name;
 	}
+	//Setter
 	public setName(String x){
 		name = x;
 	}
